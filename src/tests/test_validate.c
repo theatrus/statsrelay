07dc6e0e17eadc2aea061dbb201dc13547dc4291
Charlie Vieth <cvieth@lyft.com>
2018-12-28 15:56:05 -0700

    WIP: test_validate: add test for parsing presampling_value

--- src/tests/test_validate.c
+++ src/tests/test_validate.c
@@ -6,10 +6,7 @@
 #include "../log.h"
 #include "../validate.h"
 
-
-int main(int argc, char **argv) {
-    stats_log_verbose(1);
-
+void test_validate_stat() {
     validate_parsed_result_t result;
 
     static const char* exp1 = "a.b.c.__tag1=v1.__tag2=v2:v2:42.000|ms";
@@ -17,6 +14,25 @@ int main(int argc, char **argv) {
     assert(0 == validate_statsd(exp1, strlen(exp1), &result));
     assert(42.0 == result.value);
     assert(METRIC_TIMER == result.type);
+}
+
+void test_parse_presampling_value() {
+    validate_parsed_result_t result;
+
+    static const char* exp1 = "test.srv.req:2.5|ms|@0.2";
+
+    assert(0 == validate_statsd(exp1, strlen(exp1), &result));
+    assert(2.5 == result.value);
+    assert(0.2 == result.presampling_value);
+    assert(METRIC_TIMER == result.type);
+}
+
+int main() {
+    stats_log_verbose(1);
+
+    test_validate_stat();
+
+	test_parse_presampling_value();
 
     return 0;
 }
