bf459dd2292ed4f1d535c105c276421cd12bfe3e
Charlie Vieth <cvieth@lyft.com>
2019-01-15 23:21:59 -0500

    WIP: log: convert log functions to macros
    *Changes:*
    
    * Always assume the log function should not be called this improves
    production performance at the cost of debug performance.  In production
    we only log WARN/ERROR so performance doesn't matter when we have to log
    since the caller is already in a bad state.
    
    * Mark all log functions as "never inline"
    
    * Add the format attribute to log functions to improve compiler
    warnings.
    
    NOTE: This takes advantage of the GCC variadic macro extension, but
    works with Clang (though a warning is emitted).

--- src/log.h
+++ src/log.h
@@ -4,12 +4,18 @@
 #include <stdarg.h>
 #include <stdbool.h>
 
+#define noinline __attribute__((__noinline__))
+#define stats_printf __attribute__((__format__(__printf__, 1, 2)))
+#define unlikely(x) __builtin_expect(!!(x), 0)
+
 enum statsrelay_log_level {
     STATSRELAY_LOG_DEBUG   = 10,
     STATSRELAY_LOG_INFO    = 20,
     STATSRELAY_LOG_WARN    = 30,
     STATSRELAY_LOG_ERROR   = 40
 };
+
+// TODO (CEV): most of these should be marked 'noinline'
 
 // set verbose logging, i.e. send logs to stderr
 void stats_log_verbose(bool verbose);
@@ -19,20 +25,31 @@ void stats_log_syslog(bool syslog);
 
 void stats_set_log_level(enum statsrelay_log_level level);
 
-// variadic log function
-void stats_vlog(const char *prefix, const char *format, va_list ap);
+enum statsrelay_log_level stats_get_log_level() __attribute__((pure));
 
 // log a message
-void stats_log(const char *format, ...);
+void noinline stats_log_impl(const char *format, ...) stats_printf;
 
 // log a debug message
-void stats_debug_log(const char *format, ...);
+void noinline stats_debug_log_impl(const char *format, ...) stats_printf;
 
 // log an error message
-void stats_error_log(const char *format, ...);
+void noinline stats_error_log_impl(const char *format, ...) stats_printf;
 
 // finish logging; this ensures that the internally allocated buffer is freed;
 // it can safely be called multiple times
 void stats_log_end(void);
+
+#define stats_log(format, ...) \
+	if (unlikely(stats_get_log_level() <= STATSRELAY_LOG_INFO)) \
+		stats_log_impl(format, ##__VA_ARGS__);
+
+#define stats_error_log(format, ...) \
+	if (unlikely(stats_get_log_level() <= STATSRELAY_LOG_ERROR)) \
+		stats_error_log_impl(format, ##__VA_ARGS__);
+
+#define stats_debug_log(format, ...) \
+	if (unlikely(stats_get_log_level() <= STATSRELAY_LOG_DEBUG)) \
+		stats_debug_log_impl(format, ##__VA_ARGS__);
 
 #endif
